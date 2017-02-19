2b325854e46c3130836b38ba1307c9326ee7853f
Kickwood <tkgame001@gmail.com>
2017-02-19 15:27:07 +0000

    Version increment

--- src/Magister/Magister.php
+++ src/Magister/Magister.php
@@ -20,7 +20,7 @@ class Magister extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '2.1.4';
+    const VERSION = '2.1.5';
 
     /**
      * Indicates if the application has been bootstrapped before.
